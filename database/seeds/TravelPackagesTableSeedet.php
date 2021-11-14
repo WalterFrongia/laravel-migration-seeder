0ec27d01828da727ca044f61785ea70b560290a3
WalterFrongia <walter.frongia@libero.it>
2021-11-14 13:09:22 +0100

    Installata seeder e creato un nuovo model

---
+++ database/seeds/TravelPackagesTableSeedet.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TravelPackagesTableSeedet extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
